c9a6ea178968678cf51906f48fb2df9fbf6280b5
Muchammad Saifurrijaal <88447746+msaifurrijaal@users.noreply.github.com>
2023-02-01 21:15:11 +0700

    first commit

---
+++ app/src/main/java/com/saifurrijaal/foodrecipe/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.saifurrijaal.foodrecipe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        val navCtrl = Navigation.findNavController(this, R.id.host_fragment)
+
+        NavigationUI.setupWithNavController(bottomNav, navCtrl)
+    }
+}
